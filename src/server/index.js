c6f180e0cb93a3d2e645cecd9e37e436dba05f8a
margalencia <margalencia@yandex.ru>
2019-02-28 11:58:28 +0300

    Add proxy server to request data from coinmarketcap

---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const Koa = require('koa');
+const Router = require('koa-router');
+const axios = require('axios');
+
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+
+const app = new Koa();
+const router = new Router();
+const APP_PORT = process.env.APP_PORT || 4000;
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
+});
+
+const config = {
+  params: {
+    start: 1,
+    limit: 5
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': process.env.API_KEY
+  }
+};
+
+router.get('/api/coins', async (ctx) => {
+  const result = await axios.get(
+    'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', config
+  );
+
+  ctx.body = result.data;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(APP_PORT, () =>
+  console.log(`App listening: http://localhost:${APP_PORT}`)
+);
