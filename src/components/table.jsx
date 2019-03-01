5856129a33c25f667f8d011a97cb05ba4bd0f895
margalencia <margalencia@yandex.ru>
2019-03-01 17:40:44 +0300

    Implement sorting by Rank in Table View

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -4,38 +4,52 @@ import {
   TableHead,
   TableBody,
   TableRow,
-  TableCell
+  TableCell,
+  Paper
 } from "@material-ui/core";
 
 import { formatCurrency, formatPercent } from "./../utils/formatters";
 
-export default ({ coins }) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell>Rank</TableCell>
-        <TableCell>Name</TableCell>
-        <TableCell>Price</TableCell>
-        <TableCell>Price Change (24h)</TableCell>
-        <TableCell>Market Cap</TableCell>
-        <TableCell>Volume (24h)</TableCell>
-        <TableCell />
-      </TableRow>
-    </TableHead>
+export default ({ coins }) => {
+  const sortedCoins = coins.concat().sort((a,b)=>{
+    if (a.cmc_rank < b.cmc_rank) {
+      return -1;
+    }
+    if (a.cmc_rank > b.cmc_rank) {
+      return 1;
+    }
+    return 0;
+  });
 
-    <TableBody>
-      {coins.map(coin => (
-        <TableRow key={coin.id}>
-          <TableCell>{coin.cmc_rank}</TableCell>
-          <TableCell>{coin.name}</TableCell>
-          <TableCell>{formatCurrency(coin.quote.USD.price)}</TableCell>
-          <TableCell>
-            {formatPercent(coin.quote.USD.percent_change_24h / 100)}
-          </TableCell>
-          <TableCell>{formatCurrency(coin.quote.USD.market_cap)}</TableCell>
-          <TableCell>{formatCurrency(coin.quote.USD.volume_24h)}</TableCell>
+  return (
+  <Paper>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Rank</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Price</TableCell>
+          <TableCell>Price Change (24h)</TableCell>
+          <TableCell>Market Cap</TableCell>
+          <TableCell>Volume (24h)</TableCell>
+          <TableCell />
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-);
+      </TableHead>
+
+      <TableBody>
+        {sortedCoins.map(coin => (
+          <TableRow key={coin.id}>
+            <TableCell>{coin.cmc_rank}</TableCell>
+            <TableCell>{coin.name}</TableCell>
+            <TableCell>{formatCurrency(coin.quote.USD.price)}</TableCell>
+            <TableCell>
+              {formatPercent(coin.quote.USD.percent_change_24h / 100)}
+            </TableCell>
+            <TableCell>{formatCurrency(coin.quote.USD.market_cap)}</TableCell>
+            <TableCell>{formatCurrency(coin.quote.USD.volume_24h)}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
+)};
